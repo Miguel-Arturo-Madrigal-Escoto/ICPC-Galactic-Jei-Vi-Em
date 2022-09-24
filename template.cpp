f135b46f56ce5689539254266e7437c5fe1dbe66
miguel.madrigal7676@alumnos.udg.mx <miguel.madrigal7676@alumnos.udg.mx>
2022-09-23 21:08:11 -0500

    template for icpc contest

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#define IO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fore(i,a,b) for(int i = a, T = b; i < T; ++i)
+#define sz(s) int(s.size())
+#define pb push_back
+#define pba pop_back
+#define all(s) begin(s),end(s)
+#define ENDL '\n'
+
+using namespace std;
+
+///types
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+
+int main(){
+	cout << "hello world"<< endl;
+	return 0;
+}
