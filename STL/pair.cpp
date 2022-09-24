c1f64a13de9c97cbbb231bb4d591eb3717952bbc
Jeanette Murillo <86679271+JaneMurillo@users.noreply.github.com>
2022-09-24 01:15:20 -0500

    stl pair

---
+++ STL/pair.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    pair<int, char> miPair;
+
+    miPair.first = 100;
+    miPair.second = 'G';
+
+    cout << miPair.first << " ";
+    cout << miPair.second << endl;
+    
+    return 0;
+}
